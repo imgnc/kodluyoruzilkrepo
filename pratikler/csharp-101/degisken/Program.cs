f48a0b4fee1d8e2985b4f62413165c6ef368f155
D.D <ddedzo@gmail.com>
2021-09-17 10:14:27 +0300

    'csharp-101' egitimi '4-degiskenler' ödevi eklendi.

---
+++ pratikler/csharp-101/degisken/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace degisken
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        { 
+            int sayi = 2;
+            string metin = "2";
+            char karakter = 'A';
+            Console.WriteLine(karakter); 
+            Console.WriteLine(Convert.ToInt32(karakter)); 
+
+            int toplam = sayi + Convert.ToInt32(metin);
+            Console.WriteLine(toplam); 
+
+            DateTime tarih= DateTime.Now;
+            Console.WriteLine(tarih.ToString("yyyy-MM-dd")); 
+
+        }
+    }
+     
+}
