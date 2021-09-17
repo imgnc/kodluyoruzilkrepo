ae083d64192d58c742fa981595f2b9a2196b027e
D.D <ddedzo@gmail.com>
2021-09-17 09:55:21 +0300

    .Net Core Patikası patikası kapsamında csharp-101 eğitimindeki ilk örnek eklendi.

---
+++ pratikler/csharp-101/console-programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello, World!");
+            Console.Write("İsminizi Giriniz : ");
+            string name = Console.ReadLine();
+            Console.Write("Soyisminizi Giriniz : ");
+            string surname = Console.ReadLine();
+            Console.Write($"Merhaba {name} {surname}");
+        }
+    }
+     
+}
